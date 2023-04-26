7efb8e8cf953a0e8cad0d57084a085e0104c76a2
Apoorv <chitnisapoorv@gmail.com>
2023-04-26 16:25:44 +0530

    Initial commit

---
+++ app/src/main/java/com/mitwpu/apoorvday2/HomeActivity.java
@@ -0,0 +1,25 @@
+package com.mitwpu.apoorvday2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Button btn = (Button)findViewById(R.id.homeButton);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this, MainActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/mitwpu/apoorvday2/MainActivity.java
@@ -0,0 +1,35 @@
+package com.mitwpu.apoorvday2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import android.view.animation.AnimationUtils;
+
+public class MainActivity extends AppCompatActivity implements Runnable{
+ImageView tv1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv1=findViewById(R.id.iv1);
+        Animation an= AnimationUtils.loadAnimation(this, R.anim.sca_anim);
+        tv1.startAnimation(an);
+        MediaPlayer mp=MediaPlayer.create(this, R.raw.song);
+        mp.start();
+        Handler h = new Handler();
+        h.postDelayed(this, 16000);
+    }
+
+    @Override
+    public void run() {
+        Intent in = new Intent(this, HomeActivity.class);
+        startActivity(in);
+    }
+}
